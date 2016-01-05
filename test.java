480a943b2d0f4e092e4d1a6d457bab4659a361d2
RaeesHafeez <13004065046@umt.edu.pk>
2016-01-05 21:22:10 +0500

    unit test code file modified by raees hafeez, 13004065046@umt.edu.pk

--- test.java
+++ test.java
@@ -8,26 +8,44 @@ public class TestBST extends TestCase{
    }
    
    public void testAdd(){
-      bst.insert(8);
-      bst.insert(3);
-      bst.insert(1);
-      bst.insert(6); 
-      bst.insert(4);
-      bst.insert(7);
-      bst.insert(10);
-      bst.insert(14);
-      bst.insert(13);
-      bst.insert(13);
+      bst.insert(new integer(8));
+      bst.insert(new integer(3));
+      bst.insert(new integer(1));
+      bst.insert(new integer(6)); 
+      bst.insert(new integer(4));
+      bst.insert(new integer(7));
+      bst.insert(new integer(10));
+      bst.insert(new integer(14));
+      bst.insert(new integer(13));
+      bst.insert(new integer(13));
+
       bst.preOrderTraversal();
       assertEquals("8 3 1 6 4 7 10 14 13", systemOut().getHistory());
       systemOut().clearHistory();
+
       bst.inOrderTraversal();
       assertEquals("1 3 4 6 7 8 10 13 14", systemOut().getHistory());
-     bst.postOrderTraversal("1 4 7 6 3 13 14 10 8", systemOut().getHistory());
-
+      systemOut().clearHistory();
+        
+     bst.postOrderTraversal()
+        assertEquals("1 4 7 6 3 13 14 10 8", systemOut().getHistory());
+         systemOut().clearHistory();
    }
    
-   public void testRemove(){
+public bool testsearch(){
+
+int r;  
+bst.insert(new integer(2));
+  bst.insert(new integer(6));
+  bst.insert(new integer(9)); 
+  bst.insert(new integer(4));
+  
+assertEquals("1" ,bst.search(new integer(9));
+systemOut.clearHistory();
+
+}   
+
+public void testRemove(){
  
      bst.insert(new Integer(10));
       bst.insert(new Integer(8));
@@ -37,6 +55,6 @@ public class TestBST extends TestCase{
       bst.delete(new Integer(6));
       systemOut().clearHistory();
       bst.inOrderTraversal();
-      assertFuzzyEquals("7 8 9 10", systemOut().getHistory());
+      assertEquals("7 8 9 10", systemOut().getHistory());
    }
 }
